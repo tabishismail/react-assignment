f87d46615e37fda4ee21eb6c2c1a5a4e9d41ddab
tabish1235 <tabish554@gmail.com>
2021-09-13 23:57:51 +0500

    import of css and little styling done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import FirstQ from "./components/First.jsx";
+import SecondQ from "./components/Second.jsx";
+import UserCards from "./components/Third.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FirstQ />
+      <SecondQ />
+      <UserCards />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/First.jsx
+++ src/components/First.jsx
@@ -1,7 +1,9 @@
+import './First.css';
+
 let Name = () => {
     let name = "Hello World";
     return (
-        <div>
+        <div className='myDiv'>
             <h1>Sample 1</h1>
             <p>{name}</p>
         </div>
@@ -12,7 +14,7 @@ let Obj = () => {
         name: "Hello World",
     }
     return (
-        <div>
+        <div className='myDiv'>
             <h1>Sample 2</h1>
             <p>{object.name}</p>
         </div>
@@ -21,7 +23,7 @@ let Obj = () => {
 let Data = () => {
     let data = ['We', 'are', 'United']
     return (
-        <div>
+        <div className='myDiv'>
             <h1>Sample 3</h1>
             {data.map((v, i) => {
                 return (
@@ -34,13 +36,15 @@ let Data = () => {
     )
 }
 let List = () => {
-    let data = ['We', 'are', 'United']
+    let list = [{ name: "Hello World 1" },
+    { name: "Hello World 2" },
+    { name: "Hello World 3" }]
     return (
-        <div>
-            <h1>Sample 3</h1>
-            {data.map((v, i) => {
+        <div className='myDiv'>
+            <h1>Sample 4</h1>
+            {list.map((v, i) => {
                 return (
-                    <p>{v}</p>
+                    <p>{v.name}</p>
                 )
             })}
 
@@ -48,8 +52,41 @@ let List = () => {
         </div>
     )
 }
-export {
-    Name,
-    Obj,
-    Data
+let Complex = () => {
+    let complex = [{ company: "XYZ", jobs: ["Javascript", "React"] }, { company: 'ABC', jobs: ['AngularJs', 'Ionic'] }]
+    return (
+        <div className='myDiv'>
+            <h1>Sample 5</h1>
+            <table>
+                <tr>
+                    <th>Company</th>
+                    <th>Jobs</th>
+                </tr>
+                {complex.map((v, i) => {
+                    return (
+                        <tr>
+                            <td>{v.company}</td>
+                            <td>{v.jobs.join(" , ")}</td>
+                        </tr>
+                    )
+                })}
+            </table>
+
+        </div>
+    )
 }
+
+let FirstQ=()=>{
+    return(
+        <div className='myDiv'>
+            <h1 className='heading'>First Question</h1>
+        <Name />
+        <Obj />
+        <Data />
+        <List />
+        <Complex />
+        </div>
+    )
+}
+
+export default FirstQ
